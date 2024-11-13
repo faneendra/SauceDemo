a24c2f6704233703359bcd40c0f0d2a788841163
Vaddaypalli Faneendra <vaddaypalli_faneendra@epam.com>
2024-11-13 10:53:56 +0530

    initial commit

---
+++ POM/Login/Login.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace POM.Login
+{
+    public class Login
+    {
+        protected By UserName => By.Name("user-name");
+        protected By Password => By.XPath("//input[@name='password' and @id = 'password']");
+        protected By SubmitButton => By.Id("login-button");
+    }
+}
